97cabec7f4ea0ff0539fa2d375820bb9ece1ba9a
Vlad Eliseev <vladeliseev@MacBook-Air-Vlad.local>
2018-10-06 14:24:02 +1000

    Updates

---
+++ precache-manifest.bf357664e2cb108c1fa46e0801c63110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "540699d011c57f264162",
+    "url": "/react-tutorial/static/js/main.540699d0.chunk.js"
+  },
+  {
+    "revision": "a108004e157f4e35dd96",
+    "url": "/react-tutorial/static/js/1.a108004e.chunk.js"
+  },
+  {
+    "revision": "540699d011c57f264162",
+    "url": "/react-tutorial/static/css/main.f832e5ab.chunk.css"
+  },
+  {
+    "revision": "66ba8af91f2ba98b2a98d7faf6601fc2",
+    "url": "/react-tutorial/index.html"
+  }
+];
