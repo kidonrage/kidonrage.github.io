c9f44d2c2aa6f81fdb20e92db3fc61226ee3615e
Vladislav Eliseev <freshymanchan@gmail.com>
2019-08-01 23:57:53 +1000

    Add files via upload

--- vizov/js/common.js
+++ vizov/js/common.js
@@ -1,22 +1,78 @@
 $(function() {
 
-	$("input[type=\"phone\"]").inputmask("+7 (999) 999-99-99");
-
-	//E-mail Ajax Send
-	$("#main-form").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "../mail.php",
-			//url: "https://cdn.jsdelivr.net/gh/agragregra/uniMail/script/mail.php",
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
+	$('#parts-carousel').owlCarousel({
+		items: 4,
+		margin: 30,
+		nav: true,
+		navText: ['<span class="icon fas"></span>','<span class="icon fas"></span>'],
+		responsive: {
+			0 : {
+				items: 1
+			},
+			480 : {
+				items: 2
+			},
+			768 : {
+				items: 3
+			},
+			992: {
+				items: 4
+			}
+		}	
 	});
+
+	$('#reviews-carousel').owlCarousel({
+		items: 3,
+		margin: 30,
+		nav: true,
+		navText: ['<span class="icon fas"></span>','<span class="icon fas"></span>'],
+		responsive: {
+			0 : {
+				items: 1
+			},
+			768 : {
+				items: 2
+			},
+			992: {
+				items: 3
+			}
+		}	
+	});
+
+	$('.accordion-item-title').click(function() {
+    var $item = $(this).parents('.accordion-item');
+    
+    $item.toggleClass('accordion-item--active').siblings('.accordion-item--active').removeClass('accordion-item--active');
+  });
+
+	$('.popup-link').magnificPopup({
+		type: 'inline',
+		fixedContentPos: true,
+		removalDelay: 300,
+		mainClass: 'mfp-zoom-out'
+	});
+	
+	// Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "" && !this.className.includes('popup-link')) {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
 });
